febf180c187c9c46bd60bf9907f78772610d59d7
quentru <47418149+quentru@users.noreply.github.com>
2019-02-08 12:17:33 -0300

    Add files via upload

---
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -0,0 +1,10 @@
+function mostrar()
+{
+//tomo la edad  
+var e
+e = document.getElementById("edad").value;
+if(e >= 13 && e <= 17)
+{
+	alert("sos adolecente");
+}
+}//FIN DE LA FUNCIÓN
---
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -0,0 +1,11 @@
+ function mostrar()
+{
+//tomo la edad  
+var e
+e = document.getElementById("edad").value;
+if(e <= 13 && e >= 17)
+{
+	alert("sos adolecente");
+}
+}
+//FIN DE LA FUNCIÓN

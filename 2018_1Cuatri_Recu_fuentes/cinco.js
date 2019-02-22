e26857a1ecc0557f8775f906f2ce403bf365ae72
quentru <47418149+quentru@users.noreply.github.com>
2019-02-22 11:30:49 -0300

    Add files via upload

--- 2018_1Cuatri_Recu_fuentes/cinco.js
+++ 2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
-
+    var precioHabitacion;
+    var metodoPago;
+    var descuento;
+    var paquete;
+    var precioFinal;
+    precioHabitacion = prompt("Ingrese el precio de la habitacion");
+    metodoPago = prompt("Ingrese su Metodo de Pago");
+    paquete = prompt("Ingrese el paquete");
+    precioHabitacion=parseInt(precioHabitacion);
+    switch(metodoPago){
+        case "visa":
+        case "mercadopago":
+        descuento = precioHabitacion * 0.10;
+        break;
+        case "paypal":
+        descuento = precioHabitacion * 0.15;
+        break;
+        case "efectivo":
+        descuento = precioHabitacion * 0.2;
+        break;
+        default:
+        descuento = precioHabitacion * 0.05;
+    }
+    precioFinal = precioHabitacion - descuento;
+    alert("El precio a pagar es "+precioFinal);
 }
--- 2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,58 @@
 function mostrar()
 {
+    var importeUno;
+    var importeDos;
+    var importeTres;
+    var importeCuatro;
+    var suma;
+    var descuento ;
+    var mensaje;
+    var mayor;
+    importeUno=prompt("Ingrese un numero");
+    importeDos=prompt("Ingrese un segundo numero");
+    importeTres=prompt("Ingrese un tercer numero");
+    importeCuatro=prompt("Ingrese un cuarto numero");
+
+    importeUno=parseInt(importeUno);
+    importeDos=parseInt(importeDos);
+    importeTres=parseInt(importeTres);
+    importeCuatro=parseInt(importeCuatro);
+
+    if (importeUno>importeDos && importeUno>importeTres && importeUno>importeCuatro)
+    {
+      mayor=importeUno;
+    }else
+    {
+        if( importeDos>importeTres && importeDos>importeCuatro){
+            mayor=importeDos;
+        }else
+        {
+            if( importeTres> importeCuatro){
+                mayor=importeTres;
+            }else
+            {
+                mayor=importeCuatro;
+            }
+        }
+
+    }
+
+ 
+   
+    suma = importeCuatro + importeDos + importeTres + importeUno;
+
+    if (suma>100){
+        descuento =90;
+       
+    }else{
+        if(suma>50){           
+            descuento = 95;
+        }else{
+            descuento=115;          
+        }
+    }
+    precioFinal = suma-suma *descuento/100;
+    alert("el mayor es"+mayor +"y el importe a pagar es:"+ precioFinal);
+
 
 }

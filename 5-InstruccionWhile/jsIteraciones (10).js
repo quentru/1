6212f984e02e244e5afb2bf1e6f69cf08ceda5c9
quentru <47418149+quentru@users.noreply.github.com>
2019-02-28 12:10:57 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,52 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
+	var contador;
+	var respuesta;
+	var numeroMaximo;
+	var numeroMinimo;
+	var numeroIngresado;
+	var sumaNegativos;
+	var sumaPositivos;
+	var cantidadPositivos;
+	var cantidadNegativos;
+	//var bandera;
+	// declarar variables
 	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var respuesta='si';
+	contador=0;
+	sumaNegativos=0;
+	sumaPositivos=0;
+	cantidadNegativos=0;
+	cantidadPositivos=0;
+	//numeroMaximo=-9999;
+	//numeroMinimo=9999;
+	//bandera="es la primera"
+	while(respuesta!='no')
 	{
-		
-	
+		numeroIngresado=prompt("Ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		if (numeroIngresado==0)
+		{
+			numeroIngresado++;
+		}
+		else
+		{
+		if (numeroIngresado<0)
+		{
+			sumaNegativos=sumaNegativos+numeroIngresado;
+			cantidadNegativos++;
+		}
+		if (numeroIngresado>0)
+		{
+			sumaPositivos=sumaPositivos+numeroIngresado;
+			cantidadPositivos++;
+		}
+		respuesta=prompt("Ingrese no para salir");
 	}
-
-
-
-
-}//FIN DE LA FUNCIÓN
+	console.log(sumaNegativos);
+	console.log(sumaPositivos);
+	console.log("la cantidad de negativos es "+cantidadNegativos);
+	console.log("la cantidad de negativos es "+cantidadPositivos);
+}
+} 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,44 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador;
+	var respuesta;
+	var numeroMaximo;
+	var numeroMinimo;
+	var numeroIngresado;
+	var bandera;
 	// declarar variables
 	
 	var respuesta='si';
-
+	contador=0;
+	numeroMaximo=-9999;
+	numeroMinimo=9999;
+	bandera="es la primera"
 	while(respuesta!='no')
 	{
-		
-	
+
+		numeroIngresado=prompt("Ingrese Numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		if(bandera==0)
+		{
+			numeroMaximo=numeroIngresado;
+			numeroMinimo=numeroIngresado;
+			bandera=1
+		}
+		else
+		{
+		if (numeroMaximo<numeroIngresado)
+		{
+			numeroMaximo=numeroIngresado;
+		}
+		if(numeroMinimo>numeroIngresado){
+			numeroMinimo=numeroIngresado;
+		}
+		respuesta=prompt("Ingrese no para salir");
 	}
 
+	console.log("el maximo es "+numeroMaximo);
+	console.log("el minimo es "+numeroMinimo);
 
-
-
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA
+}

b2467c644b7a102a1d3f9ca985a60a2cad5e79c6
quentru <47418149+quentru@users.noreply.github.com>
2019-04-08 12:06:04 -0300

    Add files via upload

---
+++ Cadenas/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+    int i;
+    //char colchon[100];
+    char nombre[20]{"pAbLo"}
+    char apellido[20]{"herNANDez"}
+    char nombreCompleto[41];
+    strcpy(apellido, nombreCompleto):
+
+    return 0;
+}
